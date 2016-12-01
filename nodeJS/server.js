beaa8a2cfc5d3680058b68cd1061f666b594e05c
Mathias Gatti <mathigatti@gmail.com>
2016-12-01 14:38:33 -0300

    correccion

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,17 +1,19 @@
 var http = require("http");
 var url = require("url");
+var fs=require('fs');
 
 var server = http.createServer();
 
 server.on("request",function(req,res) {
 	var date = new Date();
-	var path = urlData["pathname"];
-	var valor=fs.readFileSync(path);
 	var current_minute= date.getMinutes();
 	var current_hour = date.getHours(); 
-	var urlData = url.parse(req.url,true);
 
-	res.end(JSON.stringify(current_hour)+":"+JSON.stringify(current_minute) + "\n" + JSON.stringify(urlData) + JSON.stringify(valor));
+	var urlData = url.parse(req.url,true);
+	var path = urlData["pathname"];
+//	var valor=fs.readFileSync(path);
+
+	res.end(JSON.stringify(current_hour)+":"+JSON.stringify(current_minute) + "\n" + JSON.stringify(urlData));
 });
 
 server.listen(process.env.PORT || 3000);
