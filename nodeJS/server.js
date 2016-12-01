a9c9c4776fe73c74f34632bcabd283f13262994c
Mathias Gatti <mathigatti@gmail.com>
2016-12-01 14:03:32 -0300

    Heroku

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,9 +1,13 @@
 var http = require("http");
+
 var server = http.createServer();
-var date = new Date();
-var current_minute= date.getMinutes(); 
-var current_hour = date.getHours(); 
+
+
 server.on("request",function(req,res) {
+	var date = new Date();
+	var current_minute= date.getMinutes(); 
+	var current_hour = date.getHours(); 
 	res.end(JSON.stringify(current_hour)+":"+JSON.stringify(current_minute) + "\n");
 });
+
 server.listen(process.env.PORT || 3000);
