f2802ce3f79e6a5f028fa5e3c9e10ce1bf1ce544
Mathias Gatti <mathigatti@gmail.com>
2016-12-01 11:56:01 -0300

    reestructuracion

---
+++ nodeJS/server.js
@@ -0,0 +1,9 @@
+var http = require("http");
+var server = http.createServer();
+var date = new Date();
+var current_minute= date.getMinutes(); 
+var current_hour = date.getHours(); 
+server.on("request",function(req,res) {
+	res.end(JSON.stringify(current_hour)+":"+JSON.stringify(current_minute) + "\n");
+});
+server.listen(3000);
