4a6797566c2e4379717704572ab379d07ffc098e
Mathias Gatti <mathigatti@gmail.com>
2016-12-01 14:20:52 -0300

    preuba

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -3,13 +3,15 @@ var url = require("url");
 
 var server = http.createServer();
 
-
 server.on("request",function(req,res) {
 	var date = new Date();
-	var current_minute= date.getMinutes(); 
+	var path = urlData["pathname"];
+	var valor=fs.readFileSync(path);
+	var current_minute= date.getMinutes();
 	var current_hour = date.getHours(); 
 	var urlData = url.parse(req.url,true);
-	res.end(JSON.stringify(current_hour)+":"+JSON.stringify(current_minute) + "\n" + JSON.stringify(urlData));
+
+	res.end(JSON.stringify(current_hour)+":"+JSON.stringify(current_minute) + "\n" + JSON.stringify(urlData) + JSON.stringify(valor));
 });
 
 server.listen(process.env.PORT || 3000);
