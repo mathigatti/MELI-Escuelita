55b51bb8364de92e5d437e9cbaa243c3bc8dafad
Mathias Gatti <mathigatti@gmail.com>
2016-12-06 15:35:20 -0300

    borro cosas

--- nodeJS/palindromos.js
+++
@@ -1,9 +0,0 @@
-var fs=require('fs');
-filename=process.argv[2];
-var palabra=filename.toString();
-for (i=0; i < parseInt(palabra.length/2); i++){
-	if (palabra[i] != palabra[palabra.length-i-1]){
-		return console.log("No es palindromo");
-	}
-}
-return console.log("Es palindromo");
